02ef68c9de61ec26a703fb68c8724ac44bbfbb15
Annie Elizabeth Freeman <annie.e.freeman@gmail.com>
2019-09-03 23:52:05 +1200

    New challenges complete

---
+++ ejs-challenge/app.js
@@ -0,0 +1,116 @@
+//jshint esversion:6
+
+
+// Require npm modules
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+
+//Starting content constants
+const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+//Create new app --> called app.js
+const app = express();
+
+//define posts
+
+let allPosts = [];
+
+
+// Tell app.js to use ejs
+app.set('view engine', 'ejs');
+
+//Tell app to use modules
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+
+
+//Define routes
+app.get("/", function(req, res) {
+
+
+  res.render("home", {
+    homeStartingContent: homeStartingContent,
+    totalPosts: allPosts
+  });
+
+
+
+});
+
+app.get("/about", function(req, res) {
+  res.render("about", {
+    aboutContent: aboutContent
+  });
+});
+
+app.get("/contact", function(req, res) {
+  res.render("contact", {
+    contactContent: contactContent
+  });
+});
+
+app.get("/compose", function(req, res) {
+  res.render("compose");
+});
+
+
+
+//Define method when user inputs text in POST request using form
+app.post("/compose", function(req, res) {
+
+
+  const post = {
+    title: req.body.postTitle,
+    body: req.body.postBody
+  };
+
+  allPosts.push(post);
+  res.redirect("/");
+
+
+
+
+
+});
+
+
+
+
+
+
+
+const test = app.get("/posts/:postName", function(req, res) {
+
+  const requestedTitle = _.lowerCase(req.params.postName);
+
+
+  allPosts.forEach(function(post) {
+    const storedTitle = _.lowerCase(post.title);
+
+    if (storedTitle === requestedTitle) {
+
+      res.render("post", {
+        title: post.title,
+        content: post.body
+
+      });
+
+
+    }
+  });
+
+
+
+});
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
